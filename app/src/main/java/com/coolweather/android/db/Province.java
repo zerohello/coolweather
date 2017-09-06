8079273990b963fb4ec2d3c7f00f0402eab29154
Zero <1151410906@qq.com>
2017-09-06 13:52:08 +0800

    start

---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,37 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/9/6.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+}
