74a038645c20752d8ca4c1b1ad8aa1898f2e281a
Zero <1151410906@qq.com>
2017-09-07 09:17:06 +0800

    增加Gson实体类

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,16 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2017/9/7.
+ */
+
+public class AQI {
+
+    public AQIcity city;
+
+    public class AQIcity{
+        public String api;
+        public String pm25;
+    }
+
+}
---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,19 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2017/9/7.
+ */
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+    @SerializedName("id")
+    public String weatherId;
+
+    public  class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
